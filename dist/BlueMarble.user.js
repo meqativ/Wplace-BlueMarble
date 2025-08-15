7560dc28ddbc1ccd5bb4621a0446d104856d0a5e
Gustavo B <gustavobc2001@hotmail.com>
2025-08-14 22:58:05 -0300

    fix: update commit hash in userscript URLs to 8b36062

--- dist/BlueMarble.user.js
+++ dist/BlueMarble.user.js
@@ -7,7 +7,7 @@
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://github.com/Seris0/Wplace-BlueMarble
-// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/147c6650582e47ffa6dc059b52cbc4a03dc46ead/dist/assets/Favicon.png
+// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/8b3606239465d90f439f7e125048bc80c738afd9/dist/assets/Favicon.png
 // @updateURL    https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @downloadURL  https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @run-at       document-start
@@ -16,7 +16,7 @@
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
-// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/147c6650582e47ffa6dc059b52cbc4a03dc46ead/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/8b3606239465d90f439f7e125048bc80c738afd9/dist/BlueMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
