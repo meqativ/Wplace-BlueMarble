852df1bcd957bd4bc38f7d78ca0603cf0b185408
Gustavo B <gustavobc2001@hotmail.com>
2025-08-15 10:04:14 -0300

    Update hash references to latest commit 6c4d368d354d44d170d740879f98abcfd1ce25ec

--- src/BlueMarble.meta.js
+++ src/BlueMarble.meta.js
@@ -7,7 +7,7 @@
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://github.com/Seris0/Wplace-BlueMarble
-// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/a2e3bf13059ce06afa01b3a4d82ba7e56037ee74/dist/assets/Favicon.png
+// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/6c4d368d354d44d170d740879f98abcfd1ce25ec/dist/assets/Favicon.png
 // @updateURL    https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @downloadURL  https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @run-at       document-start
@@ -16,7 +16,7 @@
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
-// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/a2e3bf13059ce06afa01b3a4d82ba7e56037ee74/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/6c4d368d354d44d170d740879f98abcfd1ce25ec/dist/BlueMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
