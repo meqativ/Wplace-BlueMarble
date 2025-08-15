8eeab33b25c48acc1494b0a2c70e9ff41c40b380
Gustavo B <gustavobc2001@hotmail.com>
2025-08-15 10:59:00 -0300

    Update hash references to commit with corrected crosshair effect 0ad70604cfdc24fb1a7b07395e56c618569cf05a

--- dist/BlueMarble.user.js
+++ dist/BlueMarble.user.js
@@ -7,7 +7,7 @@
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://github.com/Seris0/Wplace-BlueMarble
-// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/8b27ab116850de498f58f4cecdf16dd4e4be55a2/dist/assets/Favicon.png
+// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/0ad70604cfdc24fb1a7b07395e56c618569cf05a/dist/assets/Favicon.png
 // @updateURL    https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @downloadURL  https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @run-at       document-start
@@ -16,7 +16,7 @@
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
-// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/8b27ab116850de498f58f4cecdf16dd4e4be55a2/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/0ad70604cfdc24fb1a7b07395e56c618569cf05a/dist/BlueMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
