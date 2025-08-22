5941d4010c2dad96706fd270f79d4d2e75789bc0
Gustavo B <gustavobc2001@hotmail.com>
2025-08-22 18:55:41 -0300

    Forgo to push

--- dist/BlueMarble.user.js
+++ dist/BlueMarble.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Skirk Marble
 // @namespace    https://github.com/Seris0/
-// @version      0.87.5
+// @version      0.87.6
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       Seris0
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://github.com/Seris0/Wplace-BlueMarble
-// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/877732a28fb53057f1cb313c5ba3abbaeb0b8987/dist/assets/Favicon.png
+// @icon         https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/54c670eb9ea340c7375c87db735b8e5ca7739a5c/dist/assets/Favicon.png
 // @updateURL    https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @downloadURL  https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @run-at       document-start
@@ -18,7 +18,7 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @connect      nominatim.openstreetmap.org
-// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/877732a28fb53057f1cb313c5ba3abbaeb0b8987/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/54c670eb9ea340c7375c87db735b8e5ca7739a5c/dist/BlueMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
